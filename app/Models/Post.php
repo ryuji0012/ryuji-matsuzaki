9c30b0fa946b14ecfb8866ad5b32d1240d0ab881
ryuji0012 <joker.core@rakumail.jp>
2023-03-10 18:36:31 +0900

    適切なコミットコメント

---
+++ app/Models/Post.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Models;
+
+class Post extends Models
+{
+    use HasFactory;
+
+    public function getPaginateLimit(int $limit_count = 5)
+    {
+        return $this->orderBy('updated_at', 'DESC')->paginate($limit_count)->get();
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Postrollers;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PostCntrollers::class, 'index']);
+Route::get('/posts/{post}', [PostController::class ,'show']);
